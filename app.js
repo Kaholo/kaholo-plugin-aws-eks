e9e235bc90fc4c1e0038798b60cd8dae624ab05c
xkcm <maciejka.citko@gmail.com>
2022-02-26 12:08:22 +0100

    Add linting

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
-const { listRegions } = require("./autocomplete")
-const dayjs = require('dayjs');
+const dayjs = require("dayjs");
+const EKSToken = require("aws-eks-token");
+const { listRegions } = require("./autocomplete");
 
-async function getToken(action, settings){
-  const EKSToken = require('aws-eks-token');
-  if (!action.params.clusterName){
-    throw "not given cluster name";
+async function getToken(action, settings) {
+  if (!action.params.clusterName) {
+    throw new Error("not given cluster name");
   }
   const expires = action.params.expires || "60";
-  if (!Number.isInteger(parseFloat(expires))){
-    throw "expires must be an integer";
+  if (!Number.isInteger(parseFloat(expires))) {
+    throw new Error("expires must be an integer");
   }
 
   EKSToken.config = {
-    accessKeyId:  action.params.accessKeyId || settings.accessKeyId,
+    accessKeyId: action.params.accessKeyId || settings.accessKeyId,
     secretAccessKey: action.params.secretAccessKey || settings.secretAccessKey,
-    region: action.params.region
-  }
+    region: action.params.region,
+  };
 
   const reqTime = dayjs();
-  const token = await EKSToken.renew(action.params.clusterName, expires,
-    reqTime.utc().format('YYYYMMDDTHHmmss[Z]'));
-  const expirationTime = reqTime.add(parseInt(expires), "s").utc().format('YYYY-MM-DDTHH:mm:ss[Z]');
+  const token = await EKSToken.renew(
+    action.params.clusterName,
+    expires,
+    reqTime.utc().format("YYYYMMDDTHHmmss[Z]"),
+  );
+  const expirationTime = reqTime.add(parseInt(expires, 10), "s").utc().format("YYYY-MM-DDTHH:mm:ss[Z]");
   return {
-    "expirationTimestamp": expirationTime,
-    "token": token
-  }
+    expirationTimestamp: expirationTime,
+    token,
+  };
 }
 
 module.exports = {
   getToken,
   // autocomplete
-  listRegions
+  listRegions,
 };
-
