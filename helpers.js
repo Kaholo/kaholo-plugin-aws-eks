8f8a27fc07bb6c7445d5bbd2e12fa574984a8caf
xkcm <xkcm16@gmail.com>
2023-11-08 18:13:35 +0100

    Remove aws-eks-token library

--- helpers.js
+++ helpers.js
@@ -1,3 +1,6 @@
+const { SignatureV4 } = require("@smithy/signature-v4");
+const { Sha256 } = require("@aws-crypto/sha256-js");
+
 const { EKS_SERVICE_URL } = require("./consts");
 
 function isObjectEmpty(ob) {
@@ -30,8 +33,46 @@ function getTokenConfig(parameters) {
   };
 }
 
+async function fetchToken(params) {
+  const { clusterName } = params;
+  const tokenConfig = getTokenConfig(params);
+
+  const signer = new SignatureV4({
+    credentials: tokenConfig,
+    region: tokenConfig.region,
+    sha256: Sha256,
+  });
+
+  const request = await signer.presign(
+    {
+      headers: {
+        host: `sts.${tokenConfig.region}.amazonaws.com`,
+        "x-k8s-aws-id": clusterName,
+      },
+      hostname: `sts.${tokenConfig.region}.amazonaws.com`,
+      method: "GET",
+      path: "/",
+      protocol: "https:",
+      query: {
+        Action: "GetCallerIdentity",
+        Version: "2011-06-15",
+      },
+    },
+    { expiresIn: 0 },
+  );
+  const query = Object.keys(request?.query ?? {})
+    .map(
+      (q) => `${encodeURIComponent(q)}=${encodeURIComponent(request.query?.[q])}`,
+    )
+    .join("&");
+  const url = `https://${request.hostname}${request.path}?${query}`;
+  const token = `k8s-aws-v1.${Buffer.from(url).toString("base64url")}`;
+
+  return token;
+}
+
 module.exports = {
   isObjectEmpty,
+  fetchToken,
   roleFilter,
-  getTokenConfig,
 };
